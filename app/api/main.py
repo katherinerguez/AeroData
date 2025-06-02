a4f26c4f42be8b3c514d9eee7aba65e742e6ae13
Jennyfer2004 <jennifer.ss2004@gmail.com>
2025-06-02 12:44:32 -0400

    rectificate the url issue github.com/katherinerguez/AeroData/issues/15

--- app/api/main.py
+++ app/api/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, Depends,HTTPException , Header
 from sqlalchemy.orm import Session
 from typing import List, Optional
-from database import SessionLocal, engine
-import tablas as tablas
-import schemas as schemas
+from app.api.database import SessionLocal, engine
+import app.api.tablas as tablas
+import app.api.schemas as schemas
 import uuid
 
 
