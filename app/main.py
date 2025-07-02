01619b20f658652845ae0c123dd98170fe06484f
Katyrguez <rkatherine538@gmail.com>
2025-07-02 17:12:43 -0400

    add home page

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 from app.api.main import app as api_app
-from app.graficos.main import app as graficos_app
 from app.consultas.main import app as consultas_app
+from app.graficos.main import app as graficos_app
 
 app = FastAPI()
 
 app.mount("/api", api_app)
 app.mount("/consultas", consultas_app)
 app.mount("/graficos", graficos_app)
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
