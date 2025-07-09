c4f021d28ace69ad91f206391689a969403edbc8
Katyrguez <rkatherine538@gmail.com>
2025-07-09 15:16:43 -0400

    synchronizing the homepage with the real-time flights microservice

--- app/home/main.py
+++ app/home/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
 import httpx
 
 app = FastAPI(
@@ -15,7 +16,8 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 MICROSERVICIOS = {
     "api": "http://api:8000",
     "consultas": "http://consultas:8001",
-    "analisis": "http://graficos:8002"
+    "analisis": "http://graficos:8002",
+    "vuelos en tiempo real": 'https://realtime-wj1e.onrender.com/realtime-flights/'
 }
 
 @app.get("/", response_class=HTMLResponse)
@@ -23,26 +25,41 @@ async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/api")
-async def api_service():
-    async with httpx.AsyncClient() as client:
-        response = await client.get("http://api:8000")  
-        return response.json()
+def redirect_vuelos_en_tiempo_real():
+    """
+    Redirige a la página del microservicio de la api.
+    """
+    url = MICROSERVICIOS.get("api")
+    if not url:
+        raise HTTPException(status_code=503, detail="Api no disponible")
+    return RedirectResponse(url)
 
 
 @app.get("/consultas")
-async def consultas_service():
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(f"{MICROSERVICIOS['consultas']}")
-            return response.json()
-        except Exception:
-            raise HTTPException(status_code=503, detail="Microservicio Consultas no disponible")
-
+def redirect_vuelos_en_tiempo_real():
+    """
+    Redirige a la página del microservicio de las consultas.
+    """
+    url = MICROSERVICIOS.get("consultas")
+    if not url:
+        raise HTTPException(status_code=503, detail="Consultas bo disponibles")
+    return RedirectResponse(url)
 @app.get("/analisis")
-async def analisis_service():
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(f"{MICROSERVICIOS['analisis']}")
-            return response.json()
-        except Exception:
-            raise HTTPException(status_code=503, detail="Microservicio Análisis no disponible")
+def analisis():
+    """
+    Redirige a la página del microservicio de analisis de vuelos.
+    """
+    url = MICROSERVICIOS.get("analisis")
+    if not url:
+        raise HTTPException(status_code=503, detail="Análisis no disponibles")
+    return RedirectResponse(url)
+     
+@app.get("/vuelos en tiempo real")
+def vuelos_en_tiempo_real():
+    """
+    Redirige a la página del microservicio de vuelos en tiempo real.
+    """
+    url = MICROSERVICIOS.get("vuelos en tiempo real")
+    if not url:
+        raise HTTPException(status_code=503, detail="Vuelos en tiempo real no disponibles")
+    return RedirectResponse(url)
